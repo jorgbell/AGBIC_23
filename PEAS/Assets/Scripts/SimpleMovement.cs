d918d487c6d0d8afe61ca1a272861c976b72a712
jorgbell <jorgbell@ucm.es>
2023-07-09 02:05:51 +0200

    Escalera y groundcheck

--- PEAS/Assets/Scripts/SimpleMovement.cs
+++ PEAS/Assets/Scripts/SimpleMovement.cs
@@ -1,53 +1,92 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net;
 using UnityEngine;
+using UnityEngine.UIElements;
+using static UnityEngine.GraphicsBuffer;
 
 public class SimpleMovement : MonoBehaviour
 {
-    Rigidbody2D rb;
+    Rigidbody2D rb; Collider2D col;
     public float speed = 5.0f;
-    public Vector2 direction = new Vector2(1f, 0f);
-    bool isFalling = true;
+    public float ladderSpeed = 5.0f;
+    public Vector2 movementDirection= new Vector2(1f, 0f);
+
+    //falling logic
+    public Vector3 boxSize;
+    public float maxGroundDistance;
+    public LayerMask groundLayerMask;
+
+    //ladder behaviour
+    [HideInInspector]
+    public bool isInLadder = false;
+    Vector2 ladderTarget;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>(); col = GetComponent<Collider2D>();
     }
 
     private void FixedUpdate()
     {
-        if (!isFalling)
+        if (GroundCheck() && !isInLadder)
         {
-            direction = direction.normalized;
-            Vector2 v = direction * speed;
+            movementDirection = movementDirection.normalized;
+            Vector2 v = movementDirection * speed;
             rb.velocity = v;
         }
-        else
+        else if (!isInLadder)
         {
             Debug.Log("quieto parao");
         }
     }
-
-    private void OnCollisionEnter2D(Collision2D collision)
+    void Update()
     {
-        Debug.Log("toco hierba");
-        isFalling = false;
+        if (isInLadder)
+        {
+            float step = ladderSpeed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, ladderTarget, step);
+
+            // Comprueba si ha llegado al punto final
+            if (Vector2.Distance(transform.position, ladderTarget) < 0.001f)
+            {
+                // Detiene el movimiento
+                ExitsLadder();
+            }
+        }
 
     }
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        Debug.Log("DREAM ON");
-        isFalling = true;
 
+    public void EntersLadder(Vector3 to) {
+        isInLadder = true; ladderTarget = to;
+        rb.isKinematic = true; rb.velocity = new Vector2(0, 0); }
+    public void ExitsLadder() { isInLadder = false; col.enabled = true;  rb.isKinematic = false; }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawCube(transform.position - transform.up * maxGroundDistance, boxSize);
     }
 
+    private bool GroundCheck()
+    {
+        if(Physics2D.BoxCast(transform.position, boxSize, 0, -transform.up, maxGroundDistance, groundLayerMask))
+        {
+            Debug.Log("IN GROUND");
+            return true;
+        }
+        Debug.Log("NOT IN GROUND");
+        return false;
+
+
+    }
 
     public void InvertRotation()
     {
         Vector3 currentRotation = transform.localRotation.eulerAngles;
         currentRotation.y += 180f;
         transform.localRotation = Quaternion.Euler(currentRotation);
-        direction *= -1;
+        movementDirection *= -1;
     }
 }
---
+++ PEAS/Assets/Scripts/TriggerZoneLadder.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerZoneLadder : MonoBehaviour
+{
+    CircleCollider2D circleCollider; 
+    Vector3 previousParentScale; 
+    Vector3 newSphereScale;
+    public Transform other;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        circleCollider = gameObject.GetComponent<CircleCollider2D>();
+        FixSphereScale();
+    }
+    private void FixSphereScale()
+    {
+        //transformamos la escala del padre a la escala original
+        previousParentScale = transform.parent.localScale;
+        transform.parent.localScale = new Vector3(1, 1, 1);
+        //pillamos la escala que queremos para la esfera. Reiniciamos solo la Y, porque la escalera podra cambiar de tamano en la Y, no en lo demas
+        newSphereScale = new Vector3(transform.localScale.x, transform.localScale.y / previousParentScale.y, transform.localScale.z);
+        //reiniciamos la escala del padre
+        transform.parent.localScale = previousParentScale;
+        //reescalamos la del hijo 
+        transform.localScale = newSphereScale;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SimpleMovement s = collision.GetComponent<SimpleMovement>();
+        if (s!=null)
+        {
+            if(!s.isInLadder) s.EntersLadder(other.position);
+        }
+
+    }
+}
